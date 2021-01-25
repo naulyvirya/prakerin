4f9eeff99d92d6a25c9fb8eb303bc95d7150df67
naulyvirya <naulyhp16@gmail.com>
2021-01-25 00:32:12 -0800

    Tracking Covid-19

---
+++ resources/views/layouts/components/sidebar.blade.php
@@ -0,0 +1,71 @@
+<div class="sidebar" data-color="orange">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          COV
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          Kawal Korona
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper">
+        <ul class="nav">
+          <li class="active ">
+            <a href="/admin">
+              <i class="now-ui-icons design_app"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a href="/admin">
+              <i class="now-ui-icons objects_key-25"></i>
+              <p>Admin</p>
+            </a>
+          </li>
+          <li>
+            <a href="../admin/provinsi">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Provinsi</p>
+            </a>
+          </li>
+          <li>
+            <a href="./kota">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Kota</p>
+            </a>
+          </li>
+          <li>
+            <a href="./kecamatan">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Kecamatan</p>
+            </a>
+          </li>
+          <li>
+            <a href="./desa">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Desa</p>
+            </a>
+          </li>
+          <li>
+            <a href="./rw">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Rw</p>
+            </a>
+          </li>
+          <li>
+            <a href="./kasus">
+              <i class="now-ui-icons location_map-big"></i>
+              <p>Kasus</p>
+            </a>
+          </li>
+          <li>
+            <a href="./">
+              <i class="now-ui-icons shopping_shop"></i>
+              <p>Home</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
