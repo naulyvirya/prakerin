63a9182ac46b06b60ddf81b904bda1c1c6b2c6ce
naulyvirya <naulyhp16@gmail.com>
2021-02-01 16:47:30 -0800

    API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostsController::class, 'index']);
+Route::post('/posts/store', [PostsController::class, 'store']);
+Route::get('/posts/{id}', [PostsController::class, 'show']);
+Route::put('/posts/update/{id}', [PostsController::class, 'update']);
+Route::delete('/posts/{id?}', [PostsController::class, 'destroy']);
