2390e5af19e54f0a3a7c28035d466ef523fb7f52
naulyvirya <naulyhp16@gmail.com>
2021-02-05 16:58:22 -0800

    API DONE

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/global', [ApiController::class, 'global']);
 Route::get('/indonesia', [ApiController::class, 'indonesia']);
 Route::get('/positif', [ApiController::class, 'positif']);
 Route::get('/sembuh', [ApiController::class, 'sembuh']);
@@ -27,6 +28,18 @@ Route::get('/meninggal', [ApiController::class, 'meninggal']);
 
 Route::get('/provinsi', [ApiController::class, 'provinsi']);
 Route::get('/provinsi/{id}', [ApiController::class, 'showProvinsi']);
+
+Route::get('/kota', [ApiController::class, 'kota']);
+Route::get('/kota/{id}', [ApiController::class, 'showKota']);
+
+Route::get('/kecamatan', [ApiController::class, 'kecamatan']);
+Route::get('/kecamatan/{id}', [ApiController::class, 'showKecamatan']);
+
+Route::get('/desa', [ApiController::class, 'desa']);
+Route::get('/desa/{id}', [ApiController::class, 'showDesa']);
+
+Route::get('/rw', [ApiController::class, 'RW']);
+Route::get('/rw/{id}', [ApiController::class, 'showRW']);
 
 Route::get('/posts', [PostsController::class, 'index']);
 Route::post('/posts/store', [PostsController::class, 'store']);
