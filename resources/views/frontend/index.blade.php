0023b92add13c62a3661502402cced718078f844
naulyvirya <naulyhp16@gmail.com>
2021-02-16 16:53:01 -0800

    Nambah Js Scroll Trigger sama About

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -33,11 +33,11 @@
         </ul>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link js-scroll-trigger" href="#about">About</a>
             </li>
             <!-- {{ url('frontend/hotline') }} -->
             <li class="nav-item active">
-                <a class="nav-link" href="#">Hotline</a>
+                <a class="nav-link js-scroll-trigger" href="#dataindo">Data Kasus</a>
             </li>
             
         </ul>  
@@ -45,22 +45,100 @@
   <!-- End Header -->
   <!-- Content -->
   <br>
-  <header class="masthead d-flex">
+    <header class="masthead d-flex">
     <div class="container text-center my-auto">
       <h1 class="mb-1">KAWAL CORONA</h1>
       <h3 class="mb-5">
         <em>Coronavirus Global & Local Live Data</em>
       </h3>
-  </header>
-  <div class="container-fluid">
+    </header>
+    <section class="content-section bg-white" id="about">
+        <div class="container text-center">
             <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <div class="d-flex">
+                        <div class="col-lg-10 mx-auto">
+                            <h2 class="text-left">Apa itu Virus Corona??</h2><br>
+                            <p style="text-align:justify;" class="lead mb-3">
+                                Virus Corona atau severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2) adalah virus yang menyerang sistem pernapasan. Penyakit karena infeksi virus ini disebut COVID-19. 
+                                Virus Corona bisa menyebabkan gangguan ringan pada sistem pernapasan, 
+                                infeksi paru-paru yang berat, hingga kematian.
+                            </p>
+                            <p style="text-align:justify;" class="lead mb-3">
+                                Kasus pertama penyakit ini terjadi di kota Wuhan, Cina, pada akhir Desember 2019. Setelah itu, 
+                                COVID-19 menular antarmanusia dengan sangat cepat dan menyebar ke puluhan negara, 
+                                termasuk Indonesia, hanya dalam beberapa bulan.
+
+                                Penyebarannya yang cepat membuat beberapa negara menerapkan kebijakan untuk 
+                                memberlakukan lockdown untuk mencegah penyebaran virus Corona. 
+                                Di Indonesia, pemerintah menerapkan kebijakan Pembatasan Sosial Berskala Besar (PSBB) untuk menekan penyebaran virus ini.
+                            </p>
+                        </div>
+                        <div class="ml-auto">
+                        <br><br><br>
+                        <br><br><br>
+                            <img src="{{asset('assets/assetfrontend/assets/img/corona6.png')}}" alt="Coronavirus" 
+                            width="250" height="250">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <div class="container-fluid">
+            <h2 class="card-title text-center">
+                Data Penyebaran Covid-19 di Indonesia
+            </h2>
+            <div class="row">
+                <!-- <div class="col-lg-3 col-md-6">
+                    <div class="card-body bg-danger">
+                    <img src="{{asset('assets/assetfrontend/assets/img/sad.png')}}" style="display: block; margin: auto;" 
+                    width="50" height="50" alt="Positif"><br>
+                        <p class="text-white">TOTAL POSITIF</p>
+                        <h2 class="text-white number-font">{{ $positif }}</h2>
+                        <p class="text-white">Orang</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-body bg-success">
+                    <i><img src="{{asset('assets/assetfrontend/assets/img/happy.png')}}" style="display: block; margin: auto;"
+                     width="50" height="50" alt="Positif"></i><br>
+                        <p class="text-white">TOTAL SEMBUH</p>
+                    <h2 class="text-white mb-0 number-font">{{ $sembuh }}</h2>
+                    <p class="text-white">Orang</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-body bg-secondary">
+                    <i><img src="{{asset('assets/assetfrontend/assets/img/cry.png')}}"  style="display: block; margin: auto;"
+                    width="50" height="50" alt="Positif"></i><br>
+                        <p class="text-white">TOTAL MENINGGAL</p>
+                    <h2 class="text-white mb-0 number-font">{{ $meninggal }}</h2>
+                    <p class="text-white">Orang</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="card-body bg-secondary">
+                    <i><img src="{{asset('assets/assetfrontend/assets/img/indo.png')}}" style="display: block; margin: auto;"
+                    width="50" height="50" alt="Positif"></i><br>
+                        <p class="text-white">INDONESIA</p>
+                        <h5 class="text-white">
+                            {{number_format($positif)}}
+                            &nbsp; POSITIF, &nbsp;
+                            {{number_format($sembuh)}}
+                            &nbsp; SEMBUH, &nbsp;
+                            {{number_format($meninggal)}}
+                            &nbsp; MENINGGAL
+                        </h5>       
+                    </div>
+                </div> -->
 				<div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
-					<div class="card bg-danger img-card box-primary-shadow" style="margin-top: 20px">
+					<div class="card bg-danger img-card box-primary-shadow" style="margin-top:20px">
 						<div class="card-body">
 							<div class="d-flex">
 								<div class="text-white">
 				    				<p class="text-white mb-0">TOTAL POSITIF</p>
-                                    <p class="text-white mb-0">{{ $positif }}</p>
+                                    <p class="mb-0 number-font">{{ $positif }}</p>
 						    		<p class="text-white mb-0">ORANG</p>
                                 </div>
                                 <div class="ml-auto">
@@ -72,7 +150,7 @@
 					</div>
 				</div>
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
-						<div class="card bg-success img-card box-secondary-shadow" style="margin-top: 20px">
+						<div class="card bg-success img-card box-secondary-shadow" style="margin-top:20px">
 							<div class="card-body">
 								<div class="d-flex">
 									<div class="text-white">
@@ -89,7 +167,7 @@
 					</div>
 				</div>
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
-						<div class="card bg-dark img-card box-success-shadow" style="margin-top: 20px">
+						<div class="card bg-dark img-card box-success-shadow" style="margin-top:20px">
 							<div class="card-body">
 								<div class="d-flex">
 									<div class="text-white">
@@ -106,7 +184,7 @@
 					</div>
 				</div>
                 <div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
-						<div class="card bg-primary img-card box-success-shadow" style="margin-top: 20px">
+						<div class="card bg-primary img-card box-success-shadow" style="margin-top:20px">
 							<div class="card-body">
 								<div class="d-flex">
 									<div class="text-white">
@@ -129,10 +207,10 @@
                             </div>
                         </div>
 					</div>
-				</div>
+				</div> 
             </div>
             <br><br>
-            <div class="row row-cards">
+            <div class="row row-cards" id="dataindo">
                 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-14">
                     <div class="card">
                         <div class="card-header">
@@ -199,7 +277,9 @@
                                     @foreach($global as $data)
                                         <tr>
                                             <td> <?php echo $no++ ?></td>
-                                            <td> <?php echo $data['attributes']['Country_Region'] ?></td>
+                                            <td> 
+                                                <?php echo $data['attributes']['Country_Region'] ?>
+                                            </td>
                                             <td> 
                                                 <?php 
                                                 echo number_format($data['attributes']['Confirmed']) 
